dd215ff2a337671066a36eb87482d96b56b1c669
ParimalTank <parimaltank2001@gmail.com>
2022-02-18 10:35:54 +0530

    Frontend Part

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+import { Grid } from '@mui/material';
 import './App.css';
+import EventDetails from './components/EventDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <Grid container spacing={24}>
+        <Grid item md={2.5} ml={10}>
+            <EventDetails />
+        </Grid>
+
+        <Grid item md={2.5}>
+            <EventDetails />
+        </Grid>
+
+        <Grid item md={2.5}>
+            <EventDetails />
+        </Grid>
+
+        <Grid item md={2.5}>
+            <EventDetails />
+        </Grid>
+
+      </Grid>
+        
       </header>
     </div>
   );
---
+++ client/src/components/EventDetails.js
@@ -0,0 +1,43 @@
+import react from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+export default function EventDetails() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Event 1
+        </Typography>
+        <Typography variant="h4" component="div">
+          Cognizant
+        </Typography>
+        <Typography sx={{ mb: 15 }} color="text.secondary">
+          Charusat
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
